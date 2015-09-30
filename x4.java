dd9eb95d274e16a508efe5b4b60bc6dbfefd8c71
DaveMarques <dave.marques.1994@gmail.com>
2015-09-30 19:37:32 -0400

    Update x4.java
    Now more epileptic friendly!

--- x4.java
+++ x4.java
@@ -7,19 +7,30 @@ float buttonX = 200;
 float buttonY = 200;
 float buttonW = 100;
 float buttonH = 100;
+float backR = 100;
+float backG = 100;
+float backB = 100;
 
 void draw(){
   
+  background(backR,backG,backB);
   
-  if((mouseX < (buttonX + (buttonW/2))) &&
-     (mouseX > (buttonX - (buttonW/2))) &&
-     (mouseY < (buttonY + (buttonH/2))) &&
-     (mouseY > (buttonY - (buttonH/2))) &&
-     mousePressed )
-  {
-    background(random(0,255),random(0,255),random(0,255));
-  }
   rect(buttonX,buttonY,buttonW,buttonH);
-    
   
 }
+
+void mousePressed(){
+  
+  if((mouseX < buttonX + buttonW/2) &&
+     (mouseX > buttonX - buttonW/2) &&
+     (mouseY < buttonY + buttonH/2) &&
+     (mouseY > buttonY - buttonH/2))
+     
+  {
+    backR = random(0,255);
+    backG = random(0,255);
+    backB = random(0,255);
+  }
+  
+}
+  
